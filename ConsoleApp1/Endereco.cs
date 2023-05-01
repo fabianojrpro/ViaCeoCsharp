a451b7e64b6e5a31056480e8b6caa52687743401
Fabiano Junior <99301175+fabianojrpro@users.noreply.github.com>
2023-05-01 01:44:57 -0300

    Add files via upload

---
+++ ConsoleApp1/Endereco.cs
@@ -0,0 +1,47 @@
+﻿
+namespace ConsoleApp1
+{
+    public class Endereco
+    {
+        public string logradouro { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+
+        public Endereco(string logradouro, string bairro, string localidade, string uf)
+        {
+            this.logradouro = logradouro;
+            this.bairro = bairro;
+            this.localidade = localidade;
+            this.uf = uf;
+
+            validaCampos();
+        }
+
+        public void validaCampos()
+        {
+            if (this.logradouro == "")
+            {
+                this.logradouro = "Não encontrado";
+            }
+            if (this.bairro == "")
+            {
+                this.bairro = "Não encontrado";
+            }
+            if (this.localidade == "")
+            {
+                this.localidade = "Não encontrado";
+            }
+            if (this.uf == "")
+            {
+                this.uf = "Não encontrado";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Logradouro: {logradouro}\nBairro: {bairro}\nLocalidade: {localidade}\nUF: {uf}";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Digite o CEP que você deseja consultar: ");
+            string cep = Console.ReadLine();
+            string url = $"https://viacep.com.br/ws/{cep}/json/";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    dynamic data = JsonConvert.DeserializeObject(json);
+
+                    string logradouro = data.logradouro;
+                    string bairro = data.bairro;
+                    string localidade = data.localidade;
+                    string uf = data.uf;
+
+                    Endereco endereco = new Endereco(logradouro, bairro, localidade, uf);
+                    Console.WriteLine(endereco);
+
+                }
+                else
+                {
+                    Console.WriteLine("Não foi possível obter as informações do CEP fornecido.");
+                }
+            }
+        }
+    }
+}
